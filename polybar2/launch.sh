5a63d869034715ce547ed0016c3a1dce58d05c4e
wemm <92384868+wshadough@users.noreply.github.com>
2021-10-22 18:41:49 +0200

    added more sleep
    gotta let my boy rest

--- polybar2/launch.sh
+++ polybar2/launch.sh
@@ -8,7 +8,7 @@ killall -q polybar
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar example2 2>&1 | tee -a /tmp/polybar1.log & disown
-sleep 0.25
+sleep 1
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar example1 2>&1 | tee -a /tmp/polybar1.log & disown
 
